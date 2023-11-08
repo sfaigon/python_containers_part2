5673fbe0068bcf202a615db202684baa966cfe3f
chinobin <sfaigon@email.com>
2023-11-08 10:03:53 -0500

    complete

---
+++ python_containers_part2.py
@@ -0,0 +1,9 @@
+where_my_things_are = {
+    "phone": "pocket",
+    "keys" : "shelf",
+    "jacket" : "coat rack",
+    "guitar" : "case"
+}
+
+for key, val in where_my_things_are.items():
+    print(f"My {key} is kept {val}")
